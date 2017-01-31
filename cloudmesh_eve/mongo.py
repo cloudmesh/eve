1286f95836f2661d04228560068d363cb798b2cf
Gregor von Laszewski <laszewski@gmail.com>
2017-01-31 06:00:55 -0500

    new:usr: use pythin 3 print function

--- cloudmesh_eve/mongo.py
+++ cloudmesh_eve/mongo.py
@@ -4,9 +4,13 @@
 # https://github.com/cloudmesh/client/blob/master/cloudmesh_client/common/Shell.py
 # if commands are missing or are not working we can fix that in cloudmesh_client
 
+from __future__ import print_function
+
 import subprocess
 import commands
 import os
+
+
 
 class mongo(object):
 
@@ -15,14 +19,14 @@ class mongo(object):
         print("not yet implemented")
         #subprocess.call(mongod --port portnumber)
         os.system('sudo service mongod start')
-        print 'mongod  ------  running'
+        print ('mongod  ------  running')
 
 
     def stop():
         """stops the mongo service."""
         os.system('sudo service mongod stop')
         #subprocess.call(mongod --shutdown) 
-        print 'mongodb  ------  stopped'
+        print ('mongodb  ------  stopped')
 
 
     def status(format=None):
@@ -32,17 +36,17 @@ class mongo(object):
         """
         output = commands.getoutput('ps -A')
         if 'mongod' in output :
-            print 'mongod  ------  running'
+            print ('mongod  ------  running')
         else:
-            print 'mongod  ------  stopped'
+            print ('mongod  ------  stopped')
 
 
     def reset():
         """stops the service and deletes the database, restarts the service."""
         os.system('sudo service mongod stop')        
-        print 'mongodb  ------  stopped'
+        print ('mongodb  ------  stopped')
         
-	#print("not yet implemented")
+        #print("not yet implemented")
         pass
 
 
