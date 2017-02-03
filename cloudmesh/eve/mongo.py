f8fb1d1006d13d7c74dec42d14c37848d54b5de4
Gregor von Laszewski <laszewski@gmail.com>
2017-02-03 07:14:27 -0500

    new:dev: added some of the cahnges made by a pull request

--- cloudmesh/eve/mongo.py
+++ cloudmesh/eve/mongo.py
@@ -12,6 +12,7 @@ from cloudmesh_client.common.Shell import Shell
 from cloudmesh_client.common.util import grep
 import shutil
 import errno
+from pymongo import MongoClient
 
 def log_print(msg):
     # temporarily used till we switch to real logger
@@ -25,7 +26,7 @@ class Mongo(object):
     Manage mongod service.
     """
 
-    def __init__(self, port=5000):
+    def __init__(self, port=27017):
         """
         sets up a mongo d service
         :param port: the prort number. default set to 5000
@@ -81,6 +82,7 @@ class Mongo(object):
         print(r)
         # subprocess.call(mongod --shutdown)
         log_print('stopped')
+        self.status()        
 
     def status(self, format=None):
         """returns the status of the service. if no parameter. if format
@@ -99,17 +101,28 @@ class Mongo(object):
         r = Shell.execute('mongod stop'.split(' '))
         print (r)
         log_print('stopped')
+        # client.drop_database(databasename); how is this differentfrom deleting the collection?
 
         # print("not yet implemented")
         pass
 
     def delete(self):
-        """just deletes all data in the database"""
-        raise NotImplementedError
+        """deletes all data in the database."""
+		try:
+            client = MongoClient(host='localhost', port=self.parameters['port'] )
+			db=client.get_database(database)
+			collectionsnames=db.collection_names()
+
+			for singlecollectionname in collectionsnames:
+				log_print ("deleting: " + singlecollectionname)
+				db.get_collection(singlecollectionname).remove({})
+		except:
+			log_print("problem deleting")
         pass
 
     def pid(self):
         """returns the pid of the mongo db servier"""
+        """ cloudmesh has a working code for that no need to reinvent the wheel"""
         output = Shell.ps=('-A')
         print (output)
         pid = grep("mongod").strip().split(' ')[0]
@@ -117,7 +130,7 @@ class Mongo(object):
 
     def log(self, path):
         """sets the log file to the given path"""
-        raise NotImplementedError
+        self.parameters['logpath'] = path
         pass
 
 # TODO: define test programs with nosetest
