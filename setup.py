8b45faba41b7cd0babbd3349a5ac1ebaf22bdf34
Gregor von Laszewski <laszewski@gmail.com>
2017-02-03 11:28:24 -0500

    new:dev: move files to deprecated so they do not confuse

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------- #
+# Copyright 2017, Gregor von Laszewski, Indiana University                #
+#                                                                         #
+# Licensed under the Apache License, Version 2.0 (the "License"); you may #
+# not use this file except in compliance with the License. You may obtain #
+# a copy of the License at                                                #
+#                                                                         #
+# http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                         #
+# Unless required by applicable law or agreed to in writing, software     #
+# distributed under the License is distributed on an "AS IS" BASIS,       #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.#
+# See the License for the specific language governing permissions and     #
+# limitations under the License.                                          #
+# ------------------------------------------------------------------------#
+
 from setuptools import find_packages, setup
 import io
 
@@ -52,4 +69,9 @@ setup \
     ],
     zip_safe=False,
     namespace_packages=['cloudmesh'],
+    entry_points={
+        'console_scripts': [
+            'cms = cloudmesh.command.shell:main',
+        ],
+    },
 )
