34c78b198980c5328f34d06c12bff90719f8f4ee
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-04-04 11:55:04 -0400

    chg:dev: cleanup dead code in elements

--- cloudmesh/rest/elements.py
+++ cloudmesh/rest/elements.py
@@ -18,23 +18,3 @@ class Elements(object):
 
             with open(filename, 'w') as fd:
                 json.dump(settings, fd, indent=4)
-    #
-    # def __init__(self, directory, filename):
-    #
-    #     settings = {}
-    #
-    #     for file in glob.glob(directory + "/*.json"):
-    #         print ("F", file)
-    #         content = readfile(file)
-    #
-    #         d = json.loads(content)
-    #         print ("--------")
-    #         print (d)
-    #         print ("--------")
-    #
-    #         settings.update(d)
-    #
-    #     print (json.dumps(settings, indent=4))
-    #
-    #     with open (filename, "w") as f:
-    #         f.write(json.dumps(settings, indent=4))
